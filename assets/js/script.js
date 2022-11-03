33dfc1850e89e8b016b0ab2f68376593fb4dba5f
Korey collazo <koreycollazo@gmail.com>
2022-11-03 17:47:07 -0400

    added save score and scoreboard features

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,13 @@ var timerEl = document.getElementById('time')
 var start = document.getElementById('start')
 var questionEl = document.getElementById('prompt')
 var selectionArea = document.getElementById('user-selection')
+let questionLabelEl = document.getElementById("current-question-label")
 var score = 0
 var timeLeft = 60;
+var nameValue
 i = 0;
 
+//variable to hold questions
 let questionsList = [{
     question: "which of the following isnt a language?",
     answer: ["html", "css", "javascript", "java chip frappuccino"],
@@ -32,13 +35,8 @@ let questionsList = [{
     correctAnswer: "correct"
 }]
 
-
-
-
-
 function countdown() {
     //starting time
-    
     var timeInterval = setInterval(function () {
         timeLeft--;
         timerEl.textContent = timeLeft;
@@ -46,6 +44,7 @@ function countdown() {
             clearInterval(timeInterval);
             timerEl.innerHTML = ""
             alert("game over, how well did you do?");
+            removeAllChildNodes(questionLabelEl);
             removeAllChildNodes(selectionArea);
             questionEl.textContent = "Congrats here is your score!"
             displayScore()
@@ -53,11 +52,14 @@ function countdown() {
       
         }, 1000);
 }
-
+//create quiz elements
 function displayQuestions(){
     let choices = document.createElement("ul");
     selectionArea.appendChild(choices);
     choices.setAttribute("id", "list")
+    let questionLabel = document.createElement("p")
+    questionLabel.textContent = "Question " + (i+1) + " out of 6"
+    questionLabelEl.appendChild(questionLabel)
     
 
     for(let q = 0; q < 4; q++){
@@ -74,11 +76,14 @@ function displayQuestions(){
         if(correctAnswer != selectedAnswer.innerText){
             timeLeft -= 10
             timerEl.innerHTML = timeLeft
+            incorrectResponce()
         }
         if (correctAnswer === selectedAnswer.innerText){
+            correctResponce()
             score++;
         } if (selectedAnswer.matches("button") === true) {
         removeAllChildNodes(selectionArea);
+        removeAllChildNodes(questionLabelEl);
         i++;
         } if (i < 6){
             displayQuestions();
@@ -98,17 +103,146 @@ function removeAllChildNodes(parent) {
         parent.removeChild(parent.firstChild);
     }
 }
-
+//quiz  start event
 start.addEventListener("click", function(){
     countdown();
     start.remove();
+    hideScoreboard();
     displayQuestions();
 });
+//correct notification
+function correctResponce() {
+    var responceTime = 3;
+    var responceEl = document.getElementById("correct-or-incorrect")
+    responceEl.textContent = "correct";
+    responceEl.setAttribute("style", "color: green")
+    var timeInterval = setInterval(function () {
+        responceTime--;
+        if(responceTime <= 0) {
+            clearInterval(timeInterval);
+            responceEl.innerHTML = ""
+          }
+        }, 1000);
+}
+//incorrect notification
+function incorrectResponce() {
+    var responceTime = 3;
+    var responceEl = document.getElementById("correct-or-incorrect")
+    responceEl.textContent = "incorrect";
+    responceEl.setAttribute("style", "color: red")
+    var timeInterval = setInterval(function () {
+        responceTime--;
+        if(responceTime <= 0) {
+            clearInterval(timeInterval);
+            responceEl.innerHTML = ""
+          }
+      
+        }, 1000);
+}
 
+//score saved notification
+function savedResponce() {
+    var responceEl = document.getElementById("correct-or-incorrect")
+    responceEl.textContent = "score saved!";
+    responceEl.setAttribute("style", "color: white")
+}
 
+//displays score 
 function displayScore(){
-    var scoreP= document.createElement("p")
+    document.getElementById("homepage-button").setAttribute("style", "visibility: visible")
+    var form = document.createElement("form")
+    var scoreP = document.createElement("p")
     scoreP.appendChild(document.createTextNode(score + " out of 6"))
-    scoreP.style.fontSize = "45px"
-    selectionArea.appendChild(scoreP)
-    }
+    scoreP.style.fontSize = "40px"
+    var nameP = document.createElement("p")
+    nameP.appendChild(document.createTextNode("Enter name to save score"))
+    nameP.style.fontSize = "30px"
+    var nameInput = document.createElement("input")
+    nameInput.setAttribute("id", "username")
+    var saveButton = document.createElement("button")
+    saveButton.appendChild(document.createTextNode("Save your Score"))
+    saveButton.setAttribute("id", "save")
+    selectionArea.style.paddingTop = 0
+    selectionArea.appendChild(form)
+    var formEl = document.querySelector("form")
+    document.getElementById("prompt").appendChild(scoreP)
+    formEl.appendChild(nameP)
+    formEl.appendChild(nameInput)
+    formEl.appendChild(saveButton)
+    //save score event
+    formEl.addEventListener("click", function(event){
+        event.preventDefault()
+        savedResponce()
+        var nameValue = document.getElementById("username").value
+        var scores = 
+        {
+            username: nameValue,
+            userScore: score + "/6"
+        }
+        localStorage.setItem("scores",  JSON.stringify(scores))
+        //init all scores  array
+        if (localStorage.getItem("init-data") != "true"){
+            localStorage.setItem("init-data", "true")
+        
+            var allScores = []
+        allScores.push(JSON.parse(localStorage.getItem("scores")));
+        localStorage.setItem('allScores', JSON.stringify(allScores));
+        removeAllChildNodes(document.querySelector("form"))
+        return;
+        }
+        var initData = localStorage.getItem("init-data");
+        if (initData = "true"){
+            allScores = JSON.parse(localStorage.getItem("allScores"));
+            allScores.push(JSON.parse(localStorage.getItem("scores")));
+            localStorage.setItem('allScores', JSON.stringify(allScores));
+            removeAllChildNodes(document.querySelector("form"))
+        }
+        
+    })
+}
+
+//creates scoreboard elements
+function createScoreBoard() {
+    var sbTitle = document.createElement("h2")
+    sbTitle.textContent = "Score Board"
+    document.getElementById("score-board").appendChild(sbTitle)
+    var allScores = JSON.parse(localStorage.getItem("allScores"))
+    var names = document.createElement("ul")
+    names.setAttribute("id", "user-names")
+    var allScoresLength = allScores.length
+    document.getElementById("score-board").appendChild(names)
+    
+    for(let k = 0; k < allScoresLength; k++){
+        var usernameEl = document.createElement("p")
+        usernameEl.textContent = allScores[k].username + " - " + allScores[k].userScore
+        document.getElementById("user-names").appendChild(usernameEl)
+    };  
+
+    var clearScores = document.createElement("button")
+    clearScores.setAttribute("id", "clearScores")
+    clearScores.textContent = "clear scores"
+    clearScores.addEventListener("click", function(){
+        localStorage.setItem('allScores', "[]")
+        location.reload();
+    })
+    document.getElementById("score-board").appendChild(clearScores)
+}
+
+
+
+//display scoreboard
+document.getElementById("score-button").addEventListener("click", function(){
+    createScoreBoard()
+    
+})
+
+//hide scoreboard
+function hideScoreboard(){
+    removeAllChildNodes(document.getElementById("score-board"))
+    
+}
+
+//back to homepage
+document.getElementById("homepage-button").addEventListener("click", function(){
+    location.reload();
+})
